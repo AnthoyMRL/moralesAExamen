6ad399c9c2aed3380893a227902ca246db740259
AnthonyMRL <anthony.morales.velez@udla.edu.ec>
2025-07-10 07:54:01 -0500

    Implementar modelo Movie y configuración de base de dato

---
+++ moralesAExamen/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace moralesAExamen.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        public string Genero { get; set; } = string.Empty;
+
+        public int AnioEstreno { get; set; }
+
+        public int Calificacion { get; set; }
+
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+    }
+}
